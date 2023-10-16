6adeb0f2da6253d6c61e94c85eac2383d5d281d1
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-16 14:06:37 -0400

    C-Variables,_if_else_ while

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * This program  prints the alphabet in
+ * in lowercase, followed by a new line, using only putchar two times.
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char c:
+
+    for (c = 'a'; c <= 'z'; c++)
+      putchar(c);
+
+    putchar('\n');
+
+    return (0);
+
+}
+  
