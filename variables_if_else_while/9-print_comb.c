920620e931184cec5ec58a60394a7eac49c5bebf
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-17 11:15:40 -0400

    C-Variables,_if_else_ while

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers
+ * main - Prints all combinations of single-digit numbers
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		putchar(i);
+		if (i < 57)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i < 8)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
