38baabd8151eacc8bcaba0b8fd3bb88bcaeeae26
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-17 10:34:54 -0400

    C-Variables,_if_else_ while

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, except q and e
+ * main - Prints the alphabet in lowercase, except for q and e
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char r;
+	char r;
 
-    for (r = 'a'; r <= 'z'; r++)
-    {
-        if (r != 'q' && r != 'e')
-            putchar(r);
-    }
-    putchar('\n');
-    return (0);
+	for (r = 'a'; r <= 'z'; r++)
+	{
+		if (r != 'q' && r != 'e')
+			putchar(r);
+	}
+	putchar('\n');
+	return (0);
 }
