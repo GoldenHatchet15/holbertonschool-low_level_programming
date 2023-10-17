185d2f672b3d0783a9528a960e1efbb1e4c51457
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-17 10:04:10 -0400

    C-Variables,_if_else_ while

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *
+ * 
+ *
+ *
+ *
+ *
+ */
+
+int main(){
+
+  char a;
+
+  for(r = "a", r <= "z", ++r){
+    if(r != "q" && r != "e"){
+      putchar(c);
+    }
+  }
+  putchar("\n");
+  return 0;
+}
