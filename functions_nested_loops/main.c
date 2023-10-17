75bd28f357c6998863b0d796bc8880b1ae508a6a
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-17 14:43:29 -0400

    C-Functions_nested_loops

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -10,3 +10,17 @@ int main(void)
     print_putchar();
     return (0);
 }
+
+#include "main.h"  /* Make sure this header file is included */
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();  /* This function should now be recognized */
+    return (0);
+}
+
