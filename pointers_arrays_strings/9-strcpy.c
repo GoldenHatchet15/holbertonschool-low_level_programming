14a83b8ec02ead73778ad360127bd67c9df4fbe5
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-24 14:45:56 -0400

    pointers_arrays_strings

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -10,14 +10,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
-    dest[i] = '\0';  /* Ensure the destination string is null-terminated */
-    return dest;
+	dest[i] = '\0';  /* Ensure the destination string is null-terminated */
+	return (dest);
 }
