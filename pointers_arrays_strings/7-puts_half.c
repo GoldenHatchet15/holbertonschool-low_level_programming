9d4ba2403ec4d191ba468649d7a6e40a36991edd
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-24 14:19:41 -0400

    pointers_arrays_strings

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string.
+ * @str: The string to be printed.
+ *
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+int length = 0, n, i;
+
+/* Calculate the length of the string */
+while (str[length])
+{
+length++;
+}
+
+/* Determine starting position to print from */
+if (length % 2 == 0)
+{
+n = length / 2;
+}
+else
+{
+n = (length - 1) / 2 + 1; /* Jumping one character ahead for odd lengths */
+}
+
+/* Print the second half of the string */
+for (i = n; i < length; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
