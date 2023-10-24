6c240c21756fde135e2670f94dd36ab7afcfd0e2
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-24 10:45:36 -0400

    pointers_arrays_strings

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>  /* For the NULL macro */
 
 /**
  * _strlen - Returns the length of a string.
@@ -8,14 +9,19 @@
  */
 int _strlen(char *s)
 {
-int length = 0;
-if (s == NULL)
-{
-return 0;
-}
-while (s[length] != '\0')
-{
-length++;
-}
-return length;  // Return the counted length
+    int length = 0;  /* Initialize a counter for the string length */
+
+    /* Ensure the string pointer is not null */
+    if (s == NULL)
+    {
+        return 0;  /* Return 0 length for a null string pointer */
+    }
+
+    /* Iterate over each character in the string, counting up until the null terminator */
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    return length;  /* Return the counted length */
 }
