866623eb5785e107672f6dce097bec6979b7ff51
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-24 13:46:51 -0400

    pointers_arrays_strings

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@
 void print_rev(char *s)
 {
 int len = 0;
- int i;
+int i;
 
 /* Calculate the length of the string */
 while (s[len])
@@ -23,5 +23,5 @@ for (i = len - 1; i >= 0; i--)
 _putchar(s[i]);
 }
 
- _putchar('\n');
+_putchar('\n');
 }
