5965a8612787024660653957780dc056ed1cbda3
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-25 00:42:37 -0400

    pointers_arrays_strings

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,29 +9,29 @@
  */
 int _atoi(char *s)
 {
-	int res = 0;  /* Resultant integer */
-	int sign = 1;  /* Sign of integer: 1 or -1 */
-	int started = 0;  /* Flag to track if we've started processing numbers */
+    int res = 0;  /* Resultant integer */
+    int sign = 1;  /* Sign of integer: 1 or -1 */
+    int started = 0;  /* Flag to track if we've started processing numbers */
 
-	while (*s)
-	{
-		if (*s == '-')
-			sign = -sign;
-		else if (*s >= '0' && *s <= '9')
-		{
-			started = 1;  /* Set flag to true */
-			res = (res * 10) + sign * (*s - '0');
-		}
-		else if (started)
-		{
-			/*
-			* Break loop if we've started processing numbers 
-			* and encounter a non-number character 
-			*/
-			break;
-		}
-		s++;
-	}
-	
-	return (res);
+    while (*s)
+    {
+        if (*s == '-')
+            sign = -sign;
+        else if (*s >= '0' && *s <= '9')
+        {
+            started = 1;  /* Set flag to true */
+            res = (res * 10) + sign * (*s - '0');
+        }
+        else if (started)
+        {
+            /*
+            * Break loop if we've started processing numbers 
+            * and encounter a non-number character 
+            */
+            break;
+        }
+        s++;
+    }
+    
+    return (res);
 }
