3a8a8dadc194ebb7d63aa77a89d60d51d649aafc
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-26 09:56:54 -0400

    pointers_arrays_strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *temp = dest;
+
+	/* Move the pointer to the end of dest string */
+	while (*temp != '\0')
+	{
+		temp++;
+	}
+
+	/* Append characters of src to dest */
+	while (*src != '\0')
+	{
+		*temp = *src;
+		temp++;
+		src++;
+	}
+
+	/* Append null byte at the end */
+	*temp = '\0';
+
+	return dest;
+}
