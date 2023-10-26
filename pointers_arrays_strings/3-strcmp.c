bdac10473a588d35ac6dd49946ebd36fe4b9d884
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-26 10:36:26 -0400

    pointers_arrays_strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: The first string to be compared.
  * @s2: The second string to be compared.
  *
- * Return: 
+ * Return:
  * If s1 < s2, it returns negative.
  * If s1 is the same as s2, it returns 0.
  * If s1 > s2, it returns positive.
