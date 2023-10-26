8e5aacb2881105b58bfe726a8807fbdff39f932c
GoldenHatchet15 <raphsant20@gmail.com>
2023-10-26 10:52:35 -0400

    pointers_arrays_strings

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,16 +8,16 @@
  */
 char *string_toupper(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            s[i] = s[i] - 'a' + 'A';
-        }
-        i++;
-    }
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+		i++;
+	}
 
-    return (s);
+	return (s);
 }
