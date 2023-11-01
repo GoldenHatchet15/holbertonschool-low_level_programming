56e19098797b822eb58d0b6106c295483c9ce75e
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-01 11:52:09 -0400

    C - Recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,23 +9,23 @@
  */
 void _print_rev_recursion(char *s)
 {
-    /* Base case: If the current character is a null terminator, return */
-    if (*s == '\0')
-    {
-        return;
-    }
-    
-    /* Recursively move to the next character */
-    _print_rev_recursion(s + 1);
-    
-    /* Print the current character on the way back up */
-    printf("%c", *s);
+	/* Base case: If the current character is a null terminator, return */
+	if (*s == '\0')
+	{
+		return;
+	}
+	
+	/* Recursively move to the next character */
+	_print_rev_recursion(s + 1);
+	
+	/* Print the current character on the way back up */
+	printf("%c", *s);
 }
 
 int main(void)
 {
-    char str[] = "This is a reverse string";
-    _print_rev_recursion(str);
-    printf("\n");  /* Print a newline after the reversed string */
-    return(0);
+	char str[] = "This is a reverse string";
+	_print_rev_recursion(str);
+	printf("\n");	/* Print a newline after the reversed string */
+	return(0);
 }
