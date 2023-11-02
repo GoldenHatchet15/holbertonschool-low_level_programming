e20e1b437e71a40fe063c4d25698a2a965640957
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-02 15:52:34 -0400

    Static libraries

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
 set -e
+
+# Delete old object files and static library if they exist.
+rm -f *.o liball.a
+
+# Compile all .c files in the current directory into object files (.o).
+gcc -c *.c
+
+# Create a static library 'liball.a' from all .o files.
 ar rcs liball.a *.o
