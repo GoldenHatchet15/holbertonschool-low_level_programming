a465d876e66076d7ce4a69cf552b439444a46647
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-05 02:09:40 -0400

    argc_argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "main.h"
 
 int main(int argc, char *argv[]) {
+    (void)argc; // Explicitly state that argc is unused
     if (argv[0]) {
         printf("%s\n", argv[0]);
     }
-    return (0);
+    return 0;
 }
+
