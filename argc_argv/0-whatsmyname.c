9e497a09179739382180f29797469067d143662d
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-05 02:10:47 -0400

    argc_argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    (void)argc; // Explicitly state that argc is unused
+  (void)argc; /* Explicitly state that argc is unused */
     if (argv[0]) {
         printf("%s\n", argv[0]);
     }
-    return 0;
+    return (0);
 }
 
