e5ce31e77537bb6d806cea8a5e569f9d388a67b5
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-05 02:14:58 -0400

    argc_argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: 0 (Always success)
+ */
+int main(int argc, char **argv)
+{
+    (void)argv; /* argv is unused, this is to avoid a compiler warning */
+    printf("%d\n", argc - 1); /* Print the number of actual arguments */
+    return 0;
+}
