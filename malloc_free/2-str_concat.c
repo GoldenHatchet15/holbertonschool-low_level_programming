72c8ba9b679ecb75330cf9b3a42b8628b3331867
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-08 17:14:42 -0400

    malloc_free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,35 +3,42 @@
 
 /**
  * str_concat - concatenates two strings
- * @s1: the first string
- * @s2: the second string
+ * @s1: the first string or NULL
+ * @s2: the second string or NULL
  *
- * Return: a pointer to the newly allocated space in memory containing
+ * Return: a pointer to a new allocated space in memory containing
  * the concatenated string, or NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
-	int i, j, len1, len2;
+	int i, j;
+	int len1 = 0, len2 = 0;
 
-	/* Treat NULL inputs as empty strings */
-	len1 = s1 ? 0 : -1;
-	while (s1 && s1[++len1])
-		;
-	len2 = s2 ? 0 : -1;
-	while (s2 && s2[++len2])
-		;
+	/* Calculate the length of s1 if it's not NULL */
+	if (s1)
+	{
+		while (s1[len1])
+			len1++;
+	}
 
-	/* Allocate memory for the concatenated string */
+	/* Calculate the length of s2 if it's not NULL */
+	if (s2)
+	{
+		while (s2[len2])
+			len2++;
+	}
+
+	/* Allocate memory for concatenated string including the null terminator */
 	concat_str = malloc((len1 + len2 + 1) * sizeof(char));
-	if (concat_str == NULL)
+	if (!concat_str)
 		return (NULL);
 
-	/* Copy the first string into the new memory */
+	/* Copy s1 into the new memory, treat NULL as empty string */
 	for (i = 0; s1 && s1[i]; i++)
 		concat_str[i] = s1[i];
 
-	/* Concatenate the second string */
+	/* Concatenate s2, treat NULL as empty string */
 	for (j = 0; s2 && s2[j]; j++)
 		concat_str[i + j] = s2[j];
 
