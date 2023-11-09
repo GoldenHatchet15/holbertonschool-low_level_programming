5ae01c16fd63c8841aeba8ac51fde63f7366d16d
GoldenHatchet15 <raphsant20@gmail.com>
2023-11-09 14:06:31 -0400

    preprocessor

---
+++ preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - computes the sum of x and y.
+ * @x: The first number.
+ * @y: The second number.
+ *
+ * Description: This macro simply adds the values of x and y.
+ * Parentheses around the arguments ensure the correct order of evaluation.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
