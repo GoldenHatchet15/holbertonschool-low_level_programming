f5eb8573d189d3cd27f04d473fe97552e4df3b6e
GoldenHatchet15 <raphsant20@gmail.com>
2023-12-01 13:10:30 -0400

    linked list task0

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,8 +1,23 @@
-#ifdef LIST_H
-#define LIST_H
+#ifdef LISTS_H
+#define LISTS_H
 
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 #endif /* LIST_H */
