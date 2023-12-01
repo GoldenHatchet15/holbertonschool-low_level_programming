ddda407e8479c12e89afff37eed77707b90014a7
GoldenHatchet15 <raphsant20@gmail.com>
2023-12-01 13:30:22 -0400

    linked list task1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - counts the number of elements in a list_t list
+ * @h: pointer to the start of the list
+ *
+ * Return: the number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+count++;       /* Increment count for each node */
+h = h->next;   /* Move to the next node */
+}
+
+return (count);
+}
+
