69750382e4b54a9f414bb09c47bbf18b7ca02134
GoldenHatchet15 <raphsant20@gmail.com>
2023-12-02 09:38:07 -0400

    doubly linked list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
+ * print_dlistint - prints all elements of a dlistint_t list
  * @h: pointer to the head of the doubly linked list
  *
- * Return: the number of nodes in the list
+ * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t count = 0;
+	size_t count = 0;
 
-while (h != NULL)
-{
-  printf("%d\n", h->n);  /* Assuming 'n' is the name of the data field */
-h = h->next;
-count++;
+	while (h) {
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return count;
 }
 
- return (count);
-}
