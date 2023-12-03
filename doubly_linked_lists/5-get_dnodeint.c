43a784b381b04e8a272ea9bc96021355801eb3ea
GoldenHatchet15 <raphsant20@gmail.com>
2023-12-03 12:24:44 -0400

    doubly linked list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to the head of the doubly linked list
  * @index: index of the node, starting from 0
  *
- * Return: the nth node of dlistint_t linkd list, or NULL if the node doesnt exst
+ * Return: nth node of dlistint_t linkd list, or NULL if the node doesnt exst
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
