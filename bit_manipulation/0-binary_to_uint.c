05e31a65bfeba01e157f452e84fc7774a6e7dc08
GoldenHatchet15 <raphsant20@gmail.com>
2023-12-03 21:00:21 -0400

    Bit manipulation

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,30 +10,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0;
+	unsigned int num = 0;
 
-    if (b == NULL)
-    {
-        return (0);
-    }
+	if (b == NULL)
+	{
+		return (0);
+	}
 
-    while (*b)
-    {
-        if (*b == '1')
-        {
-            num = num * 2 + 1;
-        }
-        else if (*b == '0')
-        {
-            num *= 2;
-        }
-        else
-        {
-            return (0);
-        }
-        b++;
-    }
+	while (*b)
+	{
+		if (*b == '1')
+		{
+			num = num * 2 + 1;
+		}
+		else if (*b == '0')
+		{
+			num *= 2;
+		}
+		else
+		{
+			return (0);
+		}
+		b++;
+	}
 
-    return (num);
+	return (num);
 }
-
