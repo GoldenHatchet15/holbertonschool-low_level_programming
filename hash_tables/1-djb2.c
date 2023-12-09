76f9a6d74922f3112bc9d39a974aab02d4911c3a
GoldenHatchet15 <raphsant20@gmail.com>
2023-12-09 17:18:03 -0400

    C - hash_tables

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,14 +1,14 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Implements the djb2 algorithm.
- * @str: Pointer to the string to be hashed.
- *
- * This function calculates a hash for a given string using the djb2 algorithm,
- * which is a fast, simple, and effective hashing algorithm.
- *
- * Return: The calculated hash.
- */
+* hash_djb2 - Implements the djb2 algorithm.
+* @str: Pointer to the string to be hashed.
+*
+* This function calculates a hash for a given string using the djb2 algorithm,
+* which is a fast, simple, and effective hashing algorithm.
+*
+* Return: The calculated hash.
+*/
 
 /* djb2 hash function */
 unsigned long int hash_djb2(const unsigned char *str)
@@ -18,22 +18,23 @@ int c;
 
 while ((c = *str++))
 {
-hash = ((hash << 5)+hash)+c;/* hash * 33 + c*/
+/* Corrected: Added spaces around '+' operator */
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 }
 
 return (hash);
 }
 
 /**
- * hash_table_create - Creates a new hash table.
- * @size: The size of the array in the hash table.
- *
- * This function initializes a new hash table with a given size. It allocates
- * memory for the hash table structure and sets up the internal array used for
- * storing the data.
- *
- * Return: A pointer to the newly created hash table, or NULL if memory allocation fails.
- */
+* hash_table_create - Creates a new hash table.
+* @size: The size of the array in the hash table.
+*
+* This function initializes a new hash table with a given size. It allocates
+* memory for the hash table structure and sets up the internal array used for
+* storing the data.
+*
+* Return: Pointer to newly created hash table, NULL if memory alloc fails.
+*/
 
 /* Function to create a hash table */
 hash_table_t *hash_table_create(unsigned long int size)
@@ -52,6 +53,7 @@ free(new_table);
 return (NULL);
 }
 
+/* Split the long line to conform to the line length limit */
 for (i = 0; i < size; i++)
 new_table->array[i] = NULL;
 
