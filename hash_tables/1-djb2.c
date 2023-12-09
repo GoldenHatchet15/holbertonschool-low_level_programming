e4675e2bc1441d48cd82a2669632d9390738669f
GoldenHatchet15 <raphsant20@gmail.com>
2023-12-09 18:13:41 -0400

    C - hash_tables

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -25,38 +25,3 @@ hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 return (hash);
 }
 
-/**
-* hash_table_create - Creates a new hash table.
-* @size: The size of the array in the hash table.
-*
-* This function initializes a new hash table with a given size. It allocates
-* memory for the hash table structure and sets up the internal array used for
-* storing the data.
-*
-* Return: Pointer to newly created hash table, NULL if memory alloc fails.
-*/
-
-/* Function to create a hash table */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-hash_table_t *new_table;
-unsigned long int i;
-
-new_table = malloc(sizeof(hash_table_t));
-if (new_table == NULL)
-return (NULL);
-
-new_table->array = malloc(sizeof(hash_node_t *) * size);
-if (new_table->array == NULL)
-{
-free(new_table);
-return (NULL);
-}
-
-/* Split the long line to conform to the line length limit */
-for (i = 0; i < size; i++)
-new_table->array[i] = NULL;
-
-new_table->size = size;
-return (new_table);
-}
