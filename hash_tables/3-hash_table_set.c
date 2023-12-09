0d70d828f2a63c32c6f794d71120085afdce520a
GoldenHatchet15 <raphsant20@gmail.com>
2023-12-09 17:47:36 -0400

    C - hash_tables

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,50 +9,60 @@
 * Return: 1 if it succeeded, 0 otherwise.
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+
 {
-hash_node_t *new_node;
+hash_node_t *new_node, *current_node;
 unsigned long int index;
 
-/* Check for a valid hash table, key, and non-empty key */
 if (!ht || !key || strlen(key) == 0)
 return (0);
 
-/* Compute the index for the key */
 index = key_index((const unsigned char *)key, ht->size);
 
-/* Check for collision - if there's already a node at index */
-for (hash_node_t *current_node = ht->array[index]; current_node != NULL; current_node = current_node->next) {
-if (strcmp(current_node->key, key) == 0) {
-/* Key exists, update the value */
-free(current_node->value); /* Free the old value */
-current_node->value = strdup(value); /* Duplicate and assign new value */
-if (!current_node->value)
-return (0);
-return (1);
+current_node = ht->array[index];
+while (current_node)
+
+{
+if (strcmp(current_node->key, key) == 0)
+
+{
+free(current_node->value);
+current_node->value = strdup(value);
+return (current_node->value != NULL);
 }
+current_node = current_node->next;
 }
 
-/* Create a new node */
 new_node = malloc(sizeof(hash_node_t));
 if (!new_node)
 return (0);
 
 new_node->key = strdup(key);
-if (!new_node->key) {
-free(new_node);
-return (0);
-}
+if (!new_node->key)
+return (free_node(new_node), 0);
 
 new_node->value = strdup(value);
-if (!new_node->value) {
-free(new_node->key);
-free(new_node);
-return (0);
-}
+if (!new_node->value)
+return (free_node(new_node), 0);
 
-/* Insert the new node at the beginning of the list at the computed index */
 new_node->next = ht->array[index];
 ht->array[index] = new_node;
 
 return (1);
+}
+
+/**
+* free_node - Frees a node's memory.
+* @node: The node to be freed.
+*/
+void free_node(hash_node_t *node)
+
+{
+if (node)
+
+{
+free(node->key);
+free(node->value);
+free(node);
+}
 }
