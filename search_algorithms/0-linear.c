3d3c05ca2f4389bffc075555f7ca3b5a6e7b7ed8
GoldenHatchet15 <raphsant20@gmail.com>
2024-01-18 10:57:00 -0400

    search_algorithms

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -22,9 +22,10 @@ return (-1);
 for (i = 0; i < size; i++)
 {
 printf("Value checked array[%u] = [%d]\n", (unsigned int)i, array[i]);
+
 if (array[i] == value)
 {
-  return ((int)i);
+return ((int)i);
 }
 }
 
